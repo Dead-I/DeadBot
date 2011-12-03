3b05efe2c305f37afb9f01aa4f2e7f8eaa01f5cc
gtoxic <rocky4126@hotmail.co.uk>
2011-12-03 13:41:22 +0000

    Added basic installer functionality

--- install.php
+++ install.php
@@ -1,1 +1,13 @@
 <?php
+	if (PHP_SAPI !== 'cli') {
+		writeout("\033[31mredYou can only install this way via CLI.\033[37m");
+		exit;
+	}
+	function fetchinput(){
+		return(trim(STDIN));
+	}
+	function writeout($msg){
+		echo($msg."\r\n");
+	}
+	writeout("\033[31mtest\033[0m"); //0m resets it to the normal colour of the CMD seeing as though 0 isn't a colour code.
+?>
