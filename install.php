24cc7241fbcda073e0359691d5d34d33524ae3d6
gtoxic <rocky4126@hotmail.co.uk>
2011-12-03 14:12:51 +0000

    Added a new way of resetting the terminal etc.

--- install.php
+++ install.php
@@ -1,5 +1,5 @@
 <?php
-	$normal	= "\033[0m"; //Sets to default colour of user's shell.
+	$normal	= "\033[0m"; //Sets to default colour of user's shell. - DEPRECATED - use resetcolour() instead if the user has system(); enabled.
 	$black	= "\033[30m";
 	$red	= "\033[31m";
 	$green	= "\033[32m";
@@ -9,9 +9,11 @@
 	$cyan	= "\033[36m";
 	$white	= "\033[37m";
 	if (PHP_SAPI !== 'cli') {
-		writeout("{$red}You can only install this way via CLI.{$normal}");
-		exit;
+		writeout("{$red}You can only install this way via CLI.");
+		goto end;
 	}
+	function resetcolour(){
+		system("tput sgr0");
 	function fetchinput(){
 		return(trim(fgets(STDIN)));
 	}
@@ -33,4 +35,6 @@
 	}else{
 		writeout("No");
 	}
+	end: //DO NOT TOUCH THIS LABEL PLEASE!
+		resetcolour();
 ?>
