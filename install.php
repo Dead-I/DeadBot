d39fa09733e3f2b52a97fdca53dd6e0d239f2022
gtoxic <rocky4126@hotmail.co.uk>
2011-12-03 13:55:32 +0000

    Added colours and fixed a bug with fetchinput

--- install.php
+++ install.php
@@ -1,13 +1,36 @@
 <?php
+	$normal	= "\033[0m"; //Sets to default colour of user's shell.
+	$black	= "\033[30m";
+	$red	= "\033[31m";
+	$green	= "\033[32m";
+	$brown	= "\033[33m";
+	$blue	= "\033[34m";
+	$purple = "\033[35m";
+	$cyan	= "\033[36m";
+	$white	= "\033[37m";
 	if (PHP_SAPI !== 'cli') {
-		writeout("\033[31mredYou can only install this way via CLI.\033[37m");
+		writeout("{$red}You can only install this way via CLI.{$normal}");
 		exit;
 	}
 	function fetchinput(){
-		return(trim(STDIN));
+		return(trim(fgets(STDIN)));
 	}
-	function writeout($msg){
-		echo($msg."\r\n");
+	function writeout($msg="",$newline=true){
+		if($newline){
+			echo($msg."\r\n");
+		}else{
+			echo($msg);
+		}
 	}
-	writeout("\033[31mtest\033[0m"); //0m resets it to the normal colour of the CMD seeing as though 0 isn't a colour code.
+	function question($msg){
+		writeout($msg.": ",false);
+	}
+	writeout("Have you read the license info?");
+	question("And do you agree with it? [{$cyan}y/n{$normal}]");
+	$input = fetchinput();
+	if($input == "y"){
+		writeout("Yes");
+	}else{
+		writeout("No");
+	}
 ?>
